79ec9bc07d1f1a141e70a78bb66980743b42e15e
herzog-network <75946937+herzog-network@users.noreply.github.com>
2022-04-01 14:24:17 +0200

    include decommission-node

--- destroy.sh
+++ destroy.sh
@@ -2,7 +2,7 @@
 
 name=`grep ipv4_address ./terraform.tfstate \
   | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'`
-ssh -t p9mk@$name -- "pf9ctl detach-node;pf9ctl delete-cluster -n p9mk"
+ssh -t p9mk@$name -- "pf9ctl detach-node;pf9ctl delete-cluster -n p9mk;pf9ctl decommission-node"
 docker run -v `pwd`:/tf -w /tf hashicorp/terraform:1.1.7 destroy -auto-approve
 rm -rf ./.terraform .terraform.lock.hcl terraform.tfstate*
 read -p "Delete personal files (y/n)?" choice
